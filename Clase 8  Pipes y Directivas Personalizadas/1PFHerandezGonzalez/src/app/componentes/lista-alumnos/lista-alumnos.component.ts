572beebb31779434c455dbe1981b9b9bb61033c3
Nancy Hernández <nancy_enap@hotmail.es>
2022-08-11 20:38:51 -0500

    mi primera entrega

--- Clase 8  Pipes y Directivas Personalizadas/1PFHerandezGonzalez/src/app/componentes/lista-alumnos/lista-alumnos.component.ts
+++ Clase 8  Pipes y Directivas Personalizadas/1PFHerandezGonzalez/src/app/componentes/lista-alumnos/lista-alumnos.component.ts
@@ -15,8 +15,12 @@ export interface alumno {
 
 
 const ELEMENT_DATA: alumno[] = [
-  { nombre: 'Francisco', apellido: 'González Flores', telefono: 5523362810 ,correo: 'ejemplo@hotmail.com' , curso: 'Angular'},
-  { nombre: 'Ricardo', apellido: 'Vázquez González', telefono: 5523942930 ,correo: 'ejemplo@hotmail.com' , curso: 'VueJS'}
+  { nombre: 'Francisco', apellido: 'González Flores', telefono: 5523362810 ,correo: 'ejemplo1@hotmail.com' , curso: 'Angular'},
+  { nombre: 'Ricardo', apellido: 'Vázquez González', telefono: 5523942930 ,correo: 'ejemplo2@hotmail.com' , curso: 'VueJS'},
+  { nombre: 'Jaime', apellido: 'Hernández Mendoza', telefono: 5513941930 ,correo: 'ejemplo3@hotmail.com' , curso: 'Diseño UX/UI'},
+  { nombre: 'Eduardo', apellido: 'Gómez García', telefono: 5530291802 ,correo: 'ejemplo4@hotmail.com' , curso: 'React Js'},
+  { nombre: 'Juan', apellido: 'Vázquez Torres', telefono: 5512290483 ,correo: 'ejemplo5@hotmail.com' , curso: 'Wordpress'},
+  { nombre: 'Jarmín', apellido: 'Larios Saucedo', telefono: 5529182900 ,correo: 'ejemplo6@hotmail.com' , curso: 'Python'}
   
 ]
 
